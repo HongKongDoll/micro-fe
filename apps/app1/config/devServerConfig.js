e98e7bbe47c2d1bcd58aa00a7d6a5b0a8c871230
0x0006e <yaoxc07@gmail.com>
2020-04-23 15:09:54 +0800

    chore: seperate devserver config

---
+++ apps/app1/config/devServerConfig.js
@@ -0,0 +1,17 @@
+import path from "path";
+
+// https://github.com/webpack/webpack-dev-server/blob/master/examples/api/simple/server.js
+const devServerOptions = {
+  contentBase: path.resolve(__dirname, "../build"),
+  open: true,
+  hot: true,
+  compress: true,
+  // progress: true,
+  // friendly error
+  quiet: true,
+  stats: {
+    colors: true,
+  },
+};
+
+export default devServerOptions;
--- apps/app1/scripts/start.js
+++ apps/app1/scripts/start.js
@@ -5,27 +5,13 @@ import webpack from "webpack";
 import WebpackDevServer from "webpack-dev-server";
 
 import createConfig from "../config/webpack.config";
+import devServerOptions from "../config/devServerConfig"
 
 const HOST = process.env.HOST || "0.0.0.0";
 const PORT = process.env.PORT || 9001;
 
 const webpackConfig = createConfig(process.env.NODE_ENV);
 const complier = webpack(webpackConfig);
-
-// https://github.com/webpack/webpack-dev-server/blob/master/examples/api/simple/server.js
-const devServerOptions = {
-  ...webpackConfig.devServer,
-  contentBase: path.resolve(__dirname, "../build"),
-  open: true,
-  hot: true,
-  compress: true,
-  // progress: true,
-  // friendly error
-  quiet: true,
-  stats: {
-    colors: true,
-  },
-};
 
 const devServer = new WebpackDevServer(complier, devServerOptions);
 
